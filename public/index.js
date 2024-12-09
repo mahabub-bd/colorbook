29e331622635b91fdde77c1276a2933c5bc31964
Mahabub Hossain <palashmahabub@gmail.com>
2024-12-09 21:25:33 +0600

    some fixes

--- public/index.js
+++ public/index.js
@@ -8,16 +8,16 @@ import Instragram from "https://res.cloudinary.com/mahabub-bd/image/upload/v1714
 
 import Linkedin from "https://res.cloudinary.com/mahabub-bd/image/upload/v1714143043/colorbook/icons8-linkedin-circled-48_dfkz5e.svg";
 
-import Close from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713149804/colorbook/close_r9dzv2.svg";
+import Close from "@/public/close_r9dzv2.svg";
 
-import Menu from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713149806/colorbook/menu_er9ewy.svg";
+import Menu from "@/public/menu_er9ewy.svg";
 
 import HeaderTop from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713011111/colorbook/header-top-bg_tdua4b.jpg";
 
 import HeroOne from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713016254/colorbook/hero-1_g3ceue.jpg";
 
+import TelephoneIcon from "@/public/phone_qukq4o.svg";
 import WhatWeDo from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713016114/colorbook/what-we-provide_bbuoud.png";
-import TelephoneIcon from "https://res.cloudinary.com/mahabub-bd/image/upload/v1713149806/colorbook/phone_qukq4o.svg";
 
 import AboutOne from "https://res.cloudinary.com/mahabub-bd/image/upload/v1715272884/rs_w_365_h_365_cg_true_niewgo.webp";
 import AboutTwo from "https://res.cloudinary.com/mahabub-bd/image/upload/v1715273090/rs_w_370_cg_true_m_eahzfn.png";
